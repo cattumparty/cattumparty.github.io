847becf971f80133cabb9603f00647de28a4979b
cattumparty <nelil1@protonmail.com>
2022-12-12 22:18:20 +0100

    test

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "About Cattum Party",
+        "excerpt":"Cattum Party is the only choice for Garfcord which will guarantee freedom and safety for all Garfers. Why should you vote for us? MORE CATPOSTING OPTIONS - we will bring back the catposting channel MORE SILLINESS - the amount of silly will increase dramatically BETTER GOOGAS - By voting for...","categories": ["cattum_party","cattum","about"],
+        "tags": [],
+        "url": "/cattum_party/cattum/about/2022/12/12/cattum_party.html",
+        "teaser": "/assets/images/catthem_propaganda.png"
+      }]
